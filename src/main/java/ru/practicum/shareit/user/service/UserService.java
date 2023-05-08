e98cd74de349edec5ff1f0c3eb81d34823d9deae
Dmitry Kuznetsov <d.j.kuznetsov@yandex.ru>
2023-05-08 19:45:56 +0300

    fix: code style correction #2

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -7,9 +7,13 @@ import java.util.List;
 public interface UserService {
 
 	User addUser(User user);
+
 	User updateUser(User user);
+
 	void deleteUserById(int userId);
+
 	User getUserById(int userId);
+
 	List<User> getUsers();
 
 }
