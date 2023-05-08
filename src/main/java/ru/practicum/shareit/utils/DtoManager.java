2e224f22b76502ac2fef56bc57586f9c570b0e29
Dmitry Kuznetsov <d.j.kuznetsov@yandex.ru>
2023-05-08 19:32:36 +0300

    fix: correction #1 (DtoManager buildDefaultValidatorFactory)

--- src/main/java/ru/practicum/shareit/utils/DtoManager.java
+++ src/main/java/ru/practicum/shareit/utils/DtoManager.java
@@ -5,7 +5,6 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import javax.validation.Validation;
 import javax.validation.Validator;
 import java.lang.reflect.Field;
 import java.util.List;
